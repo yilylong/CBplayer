841f20df52f8071d303b36023338faac058eb823
yilylong <yilylong@163.com>
2015-06-10 17:35:17 +0800

    增加屏幕亮度和音量手势调节

--- Progects/CBplayer/CBPlayer/src/com/chongbao/cbplayer/activity/VideoViewActivity.java
+++ Progects/CBplayer/CBPlayer/src/com/chongbao/cbplayer/activity/VideoViewActivity.java
@@ -2,6 +2,8 @@ package com.chongbao.cbplayer.activity;
 
 import io.vov.vitamio.widget.VideoView;
 import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -18,6 +20,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.chongbao.cbplayer.R;
+import com.chongbao.cbplayer.utils.BrightnessUtil;
 import com.chongbao.cbplayer.utils.MeasureUtil;
 import com.chongbao.cbplayer.view.CBProgressBar;
 
@@ -47,6 +50,8 @@ public class VideoViewActivity extends Activity implements OnClickListener{
 	private boolean onTouch = false;
 	private int mScreenW,mScreenH;
 	private int mTouchSlop;
+	private AudioManager mAudioManager;
+	
 	/**定时隐藏控制层**/
 	private Handler mDismissHander = new Handler(){
 		public void handleMessage(android.os.Message msg) {
@@ -109,26 +114,32 @@ public class VideoViewActivity extends Activity implements OnClickListener{
 			downX = (int) event.getX();
 			downY = (int) event.getY();
 			break;
+		case MotionEvent.ACTION_POINTER_2_DOWN:
+			return true;
 		case MotionEvent.ACTION_MOVE:
 			onTouch = true;
+			int dx = (int) (event.getX()-downX);
+			int dy = (int) (event.getY()-downY);
 			// 判断X落点在屏幕的左边还是右边
 			if(downX<=mScreenW*1/4){// 在屏幕左边
 				Log.i(TAG, "在屏幕左边");
-				int dx = (int) (event.getX()-downX);
-				int dy = (int) (event.getY()-downY);
 				if(Math.abs(dx)<=mTouchSlop&&Math.abs(dy)>mTouchSlop&&dy<0){// 向上滑
-					Log.i(TAG, "dx=="+dx+"---dy="+dy+"-----向上滑");
+					judgeBrightness(dy);
 				}else if(Math.abs(dx)<=mTouchSlop&&Math.abs(dy)>mTouchSlop&&dy>0){//向下滑
-					Log.i(TAG, "dx=="+dx+"---dy="+dy+"-----向下滑");
+					judgeBrightness(dy);
 				}
 			}else if(downX>=mScreenW*3/4){// 在屏幕右边
 				Log.i(TAG, "在屏幕右边");
-				int dx = (int) (event.getX()-downX);
-				int dy = (int) (event.getY()-downY);
-				if(dx<=mTouchSlop&&dy<0){// 向上滑
-					Log.i(TAG, "dx=="+dx+"---dy="+dy+"-----向上滑");
-				}else if(dx<=mTouchSlop&&dy>0){//向下滑
-					Log.i(TAG, "dx=="+dx+"---dy="+dy+"-----向下滑");
+				if(Math.abs(dx)<=mTouchSlop&&Math.abs(dy)>mTouchSlop&&dy<0){// 向上滑
+					judgeVolume(dy);
+				}else if(Math.abs(dx)<=mTouchSlop&&Math.abs(dy)>mTouchSlop&&dy>0){//向下滑
+					judgeVolume(dy);
+				}
+			}else{//在屏幕中间
+				// 判断是横向滑动还是竖向滑动
+				if(Math.abs(dy)<=mTouchSlop&&Math.abs(dx)>mTouchSlop&&Math.abs(dx)>50){// 横向滑动
+					// TODO 快进/快退
+					Log.i(TAG, "dx=="+dx+"---dy="+dy+"-----中间滑动");
 				}
 			}
 			
@@ -147,6 +158,98 @@ public class VideoViewActivity extends Activity implements OnClickListener{
 		return super.onTouchEvent(event);
 	}
 	
+	/**
+	 * 调整亮度(activity 的亮度值是0-1的浮点数)
+	 * @param dy
+	 */
+	private void judgeBrightness(int dy) {
+		float maxBirghtness = 100f;
+		// 先关闭系统的自动亮度调节
+		BrightnessUtil.setBrightnesMode(this, 0);
+		float currentBrightness = BrightnessUtil.getCurrentActivityBrightness(this);
+		showBrightnessControl(currentBrightness);
+		float percent = Math.abs((float)dy)/mScreenH;
+		if(dy<0){// 亮度增加
+			if(currentBrightness<1){
+				currentBrightness+=percent;
+				if(currentBrightness>=1){
+					currentBrightness=1;
+				}
+				BrightnessUtil.setBrightness(this, currentBrightness);
+				mCBProgressBar.setProgress((int)(maxBirghtness*currentBrightness));
+			}
+		}else{// 亮度减
+			if(currentBrightness>0.05){
+				currentBrightness-=percent;
+				if(currentBrightness<=0.05){
+					currentBrightness=0.05f;
+				}
+				BrightnessUtil.setBrightness(this, currentBrightness);
+				mCBProgressBar.setProgress((int)(maxBirghtness*currentBrightness));
+			}
+		}
+		
+	}
+
+
+	/**
+	 * 调整音量
+	 * @param dy
+	 */
+	private void judgeVolume(int dy) {
+		if(mAudioManager==null){
+			mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		}
+		showVolumeControl(mAudioManager);
+		int maxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+		int average = mScreenH/15;
+		if(dy<0){// 音量增大
+			if(currentVolume<maxVolume){
+				currentVolume+=Math.floor(Math.abs(dy)/average);
+				if(currentVolume>=maxVolume){
+					currentVolume = maxVolume;
+				}
+			}
+			mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0);
+			mCBProgressBar.setProgress(currentVolume);
+		}else{// 音量-
+			if(currentVolume>0){
+				currentVolume-=Math.floor(Math.abs(dy)/average);
+				if(currentVolume<=0){
+					currentVolume = 0;
+				}
+			}
+			mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0);
+			mCBProgressBar.setProgress(currentVolume);
+		}
+	}
+
+	/**
+	 * 显示音量调节
+	 * @param mAudioManager
+	 */
+	private void showVolumeControl(AudioManager mAudioManager){
+		int maxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+		mCBProgressBar.setMax(maxVolume);
+		mCBProgressBar.setProgress(currentVolume);
+		mControlResultIcon.setImageResource(R.drawable.icon_volume);
+		mControlResultRLayout.setVisibility(View.VISIBLE);
+		mCBProgressBar.setVisibility(View.VISIBLE);
+	}
+	/**
+	 * 显示亮度调节
+	 * @param currentBrightness
+	 */
+	private void showBrightnessControl(float currentBrightness){
+		float maxVolume = 100;
+		mCBProgressBar.setMax((int)maxVolume);
+		mCBProgressBar.setProgress((int)(maxVolume*currentBrightness));
+		mControlResultIcon.setImageResource(R.drawable.icon_brightness);
+		mControlResultRLayout.setVisibility(View.VISIBLE);
+		mCBProgressBar.setVisibility(View.VISIBLE);
+	}
 	
 	/**
 	 * 显示控制层视图
@@ -158,6 +261,7 @@ public class VideoViewActivity extends Activity implements OnClickListener{
 
 	private void dismissControlFrame() {
 		mControlResultRLayout.setVisibility(View.GONE);
+		mCBProgressBar.setVisibility(View.INVISIBLE);
 		mControlRLayout.setVisibility(View.INVISIBLE);
 	}
 	/**
